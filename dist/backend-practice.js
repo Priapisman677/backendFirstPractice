cbe715f0cf812a81ef7d5ca69a6810d48dec14a4
Miguel <expeliam02@hotmail.com>
2024-11-15 21:00:47 -0700

    First commitment, this should go to me

---
+++ dist/backend-practice.js
@@ -0,0 +1,11 @@
+"use strict";
+const xhr = new XMLHttpRequest();
+//*This sents the message:
+xhr.open('GET', 'https://supersimplebackend.dev');
+// xhr.open('GET' as string, 'https://priapisman677.github.io/mother-load-practice-project/' as string);
+//*This sends the message:
+xhr.send();
+console.log('Test');
+console.log('Test2');
+console.log('Test3');
+console.log('Test4');
---
+++ src/backend-practice.ts
@@ -0,0 +1,15 @@
+const xhr: XMLHttpRequest = new XMLHttpRequest();
+//*This sents the message:
+
+xhr.open('GET', 'https://supersimplebackend.dev');
+
+// xhr.open('GET' as string, 'https://priapisman677.github.io/mother-load-practice-project/' as string);
+
+
+//*This sends the message:
+xhr.send();
+
+console.log('Test')
+console.log('Test2')
+console.log('Test3')
+console.log('Test4')
